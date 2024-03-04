53e10ae4d6807c54fc4afa300c54f7ca2ed4c849
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2024-03-05 01:56:12 +1100

    debug

--- src/editors/array.js
+++ src/editors/array.js
@@ -506,6 +506,9 @@ export class ArrayEditor extends AbstractEditor {
       })
     }
 
+    // eslint-disable-next-line no-console
+    console.log('controls holder?', this.rows[i].controls, this.rows[i].title_controls, this.rows[i].array_controls)
+
     const controlsHolder = this.rows[i].controls || this.rows[i].title_controls || this.rows[i].array_controls
 
     /* Buttons to delete row, move row up, and move row down */
